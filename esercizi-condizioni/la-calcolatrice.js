105c896f9447e17f0505e081019ebe132a183119
Crazyguitarist93 <84730494+Crazyguitarist93@users.noreply.github.com>
2021-06-01 23:17:51 +0200

    Update la-calcolatrice.js

--- esercizi-condizioni/la-calcolatrice.js
+++ esercizi-condizioni/la-calcolatrice.js
@@ -28,3 +28,45 @@
 
   http://www.imparareaprogrammare.it
 */
+
+let numero1 = prompt('Inserisci numero 1');
+let numero2 = prompt('Inserisci numero2');
+let operazione = prompt('inserisci l\'operazione da eseguire');
+
+
+function somma (numero1, numero2) {
+  return numero1 + numero2
+}
+
+
+function moltiplicazione (numero1, numero2) {
+  return numero1 * numero2
+}
+
+
+function divisione (numero1, numero2) {
+  return numero1 / numero2
+}
+
+function modulo (numero1, numero2) {
+  return numero1 % numero2
+}
+
+function pow (numero1, numero2) {
+  return Math.pow(numero1, numero2);
+}
+
+function media (numero1, numero2) {
+  return (numero1 + numero2) / 2;
+}
+
+switch(operazione) {
+  case 1:
+    operazione == somma 
+    somma(numero1, numero2)
+    console.log("
+    break;
+ 
+    
+    
+
